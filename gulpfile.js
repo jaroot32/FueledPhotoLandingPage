81f94c5f18f57e0a2d963860a62ee8678ffb3d9c
jaroot32 <jonathanroot15@gmail.com>
2018-05-18 20:49:53 -0700

    changed nav background colors

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 
+
 gulp.task('sass', function () {
     gulp.src('scss/styles.scss')
         .pipe(sass({includePaths: ['scss']}))
@@ -26,3 +27,4 @@ gulp.task('browser-sync', function() {
 gulp.task('default', ['sass', 'browser-sync'], function () {
     gulp.watch("scss/*.scss", ['sass']);
 });
+
