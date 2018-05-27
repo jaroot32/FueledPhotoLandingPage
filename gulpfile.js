7b014eb076aae98c1401041037c81aa2ac4a852b
jaroot32 <jonathanroot15@gmail.com>
2018-05-26 20:19:25 -0700

    added jekyll and truck page

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,52 @@
+'use strict';
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
+/* var concat = require('gulp-concat'); */
+var browserSync = require('browser-sync').create();
 
+/* gulp.task("concatScripts", function(){
+    gulp.src(['bower_components/jquery/dist/jquery.min.js', 'bower_components/masonry/dist/masonry.pkgd.min.js', 'bower_components/scrollreveal/dist/scrollreveal.min.js', 'js/app.js'])
+    .pipe(concat("app.js"))
+    .pipe(gulp.dest("js"));
+}); */
+
+gulp.task("minifyScripts", function () {
+    gulp.src("js/main.js")
+        .pipe(gulp.dest('js'));
+});
 
 gulp.task('sass', function () {
-    gulp.src('scss/styles.scss')
-        .pipe(sass({includePaths: ['scss']}))
+    return gulp.src('_scss/styles.scss')
+        .pipe(sass({
+            includePaths: ['scss'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('_site/css'))
+        .pipe(browserSync.reload({stream:true}))
         .pipe(gulp.dest('css'));
 });
 
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
 
-// Static server
-gulp.task('browser-sync', function() {
-	var files = [
-		'css/*.css',
-		'index.html',
-		'js/*.js'
-	];
-    browserSync.init(files, {
-        server: {
-            baseDir: "./"
-        }
-    });
+gulp.task('watch', function () {
+    gulp.watch('_scss/*.scss', ['sass']);
+    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
-gulp.task('default', ['sass', 'browser-sync'], function () {
-    gulp.watch("scss/*.scss", ['sass']);
-});
+// Task for building blog when something changed:
+// gulp.task('build', shell.task(['bundle exec jekyll build --watch']));
+// Or if you don't use bundle:
+// gulp.task('build', shell.task(['jekyll build --watch']));
 
+// Task for serving blog with Browsersync
+gulp.task('serve', function () {
+    browserSync.init({server: {baseDir: '_site/'}});
+    // Reloads page when some of the already built files changed:
+    gulp.watch('_site/**/*.*').on('change', browserSync.reload);
+});
+gulp.task('default', ['build', 'serve']);
