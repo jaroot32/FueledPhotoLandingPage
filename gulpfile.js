16cb71b33d33ed80d365655e3a3b71670267b3b6
jaroot32 <jonathanroot15@gmail.com>
2018-05-29 22:59:28 -0700

    mobile pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,13 @@
-'use strict';
-
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-/* var concat = require('gulp-concat'); */
+var shell = require('gulp-shell');
 var browserSync = require('browser-sync').create();
 
-/* gulp.task("concatScripts", function(){
-    gulp.src(['bower_components/jquery/dist/jquery.min.js', 'bower_components/masonry/dist/masonry.pkgd.min.js', 'bower_components/scrollreveal/dist/scrollreveal.min.js', 'js/app.js'])
-    .pipe(concat("app.js"))
-    .pipe(gulp.dest("js"));
-}); */
-
-gulp.task("minifyScripts", function () {
-    gulp.src("js/main.js")
-        .pipe(gulp.dest('js'));
-});
-
-gulp.task('sass', function () {
-    return gulp.src('_scss/styles.scss')
-        .pipe(sass({
-            includePaths: ['scss'],
-            onError: browserSync.notify
-        }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/css'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('css'));
-});
-
-/**
- * Watch scss files for changes & recompile
- * Watch html/md files, run jekyll & reload BrowserSync
- */
-
-gulp.task('watch', function () {
-    gulp.watch('_scss/*.scss', ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
-});
-
 // Task for building blog when something changed:
-// gulp.task('build', shell.task(['bundle exec jekyll build --watch']));
-// Or if you don't use bundle:
-// gulp.task('build', shell.task(['jekyll build --watch']));
+gulp.task('build', shell.task(['bundle exec jekyll serve']));
+// If you don't use bundle:
+// gulp.task('build', shell.task(['jekyll serve']));
+// If you use  Windows Subsystem for Linux (thanks @SamuliAlajarvela):
+// gulp.task('build', shell.task(['bundle exec jekyll serve --force_polling']));
 
 // Task for serving blog with Browsersync
 gulp.task('serve', function () {
@@ -49,4 +15,5 @@ gulp.task('serve', function () {
     // Reloads page when some of the already built files changed:
     gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
+
 gulp.task('default', ['build', 'serve']);
