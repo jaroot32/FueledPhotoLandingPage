d898c745b323761ade13d26b1508427c7c35872c
jaroot32 <jonathanroot15@gmail.com>
2018-05-31 04:14:00 -0700

    underline for links

--- js/main.js
+++ js/main.js
@@ -2,12 +2,16 @@ $(function() {
 
 
     // toggle menu
-   $('.menuDrop').on('click', function(e) {
+  $('.menuDrop').on('click', function(e) {
   // Prevent link from jumping to the top of the page
   e.preventDefault();
   // If menu is already showing, slide it up. Otherwise, slide it down.
   $('.menu').toggleClass('slide-down');
 });
+
+ $(window).on("scroll", function() {
+        $(window).scrollTop() > 5 ? $("header").addClass("active") : $("header").removeClass("active")
+    });
 
 
     // fullpage customization
